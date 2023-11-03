f00367910cd9909e9dede0034117f5b5cda4db7e
Balandzin_Anton <balandzin2602@gmail.com>
2023-11-03 18:00:08 +0300

    git init

---
+++ HW_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2
+//
+//  Created by Антон Баландин on 3.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
