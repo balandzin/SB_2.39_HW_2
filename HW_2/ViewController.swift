7689589c63b5215a13f6c80ec9dc441bf27897b1
Balandzin_Anton <balandzin2602@gmail.com>
2023-11-03 19:08:10 +0300

    add IBOutlet

--- HW_2/ViewController.swift
+++ HW_2/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redRoundView: UIView!
+    @IBOutlet var yellowRoundView: UIView!
+    @IBOutlet var greenRoundView: UIView!
+    
+    
+    @IBOutlet var changeColorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redRoundView.layer.cornerRadius = 50
+        yellowRoundView.layer.cornerRadius = 50
+        greenRoundView.layer.cornerRadius = 50
+        
+       
     }
 
 
+    @IBAction func changeTrafficColor(_ sender: UIButton) {
+        changeColorButton.setTitle("NEXT", for: .normal)
+    
+    }
+    
 }
 
